21da091006b9c7c58c9686a2f1af99e6c8fbfa91
Gwinyai <gnyatsoka@gmail.com>
2023-02-22 11:41:57 +0200

    Initial Commit

---
+++ ToDoListAppNew/TaskTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TaskTableViewCell.swift
+//  ToDoListAppNew
+//
+//  Created by Gwinyai Nyatsoka on 21/2/2023.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var checkmarkButton: UIButton!
+    
+    @IBAction func checkmarkButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
---
+++ ToDoListAppNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListAppNew
+//
+//  Created by Gwinyai Nyatsoka on 21/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
